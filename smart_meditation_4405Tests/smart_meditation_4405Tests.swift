5341b3b32862b413cf7197c9b6098d8537df3196
Crowdbotics <team@crowdbotics.com>
2019-06-08 18:39:44 +0000

    initial commit

---
+++ smart_meditation_4405Tests/smart_meditation_4405Tests.swift
@@ -0,0 +1,34 @@
+//
+//  smart_meditation_4405Tests.swift
+//  smart_meditation_4405Tests
+//
+//  Created by Dan on 10/23/18.
+//  Copyright © 2018 Crowdbotics. All rights reserved.
+//
+
+import XCTest
+@testable import smart_meditation_4405
+
+class smart_meditation_4405Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
